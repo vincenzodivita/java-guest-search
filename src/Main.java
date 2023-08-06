537e3e82ffba2a1c0f72d77dd8516377abe2fc11
vincenzodivita <divitavincenzo@icloud.com>
2023-08-06 11:41:24 +0200

    Add method for serach name in the array

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,23 @@ public class Main {
         // Guests list
         String[] guests = {"Irene", "Giancluca", "Andrea", "Vincenzo", "Gloria", "Giulia"};
 
-        // Print Result
-        for (String guest : guests) {
+        // Guest to find
+        String searchName = "Vincenzo";
 
-            System.out.println(guest);
-            
+        // Initial search state
+        boolean result = false;
+
+        // Search fn
+        for (String guest : guests) {
+            if (guest == searchName) {
+                result = true;
+                break;
+            }
         }
+
+        // Print Result
+        System.out.println((result == true)
+        ? searchName + " è tra gli invitati"
+        : searchName + " è tra gli invitati");
     }
 }
